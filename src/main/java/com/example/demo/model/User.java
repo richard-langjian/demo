3af57fc5b9d3303f5433bc3908e8d93000ec8010
richard-langjian <langjian@staff.tixa.com>
2020-09-19 15:58:15 +0800

    add a userbean

---
+++ src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,19 @@
+package com.example.demo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    private String userId;
+    private String userName;
+    private String passWord;
+    private Date createTime;
+}
