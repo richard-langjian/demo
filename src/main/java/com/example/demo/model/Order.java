e3ab502f03a1c6dafb3493e21802e0b1cc51316e
richard-langjian <langjian@staff.tixa.com>
2020-09-19 15:59:42 +0800

    add a orderbean

---
+++ src/main/java/com/example/demo/model/Order.java
@@ -0,0 +1,19 @@
+package com.example.demo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order {
+    private String orderId;
+    private String orderName;
+    private String byUser;
+    private Date createTime;
+}
